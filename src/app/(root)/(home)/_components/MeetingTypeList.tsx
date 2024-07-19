dee8ad604d5f3feb5d6e604195fe925b3a1a05a4
junihoj <ezeonyekachukwu98@gmail.com>
2024-07-19 15:34:32 +0100

    inital commit, project structure/layout

---
+++ src/app/(root)/(home)/_components/MeetingTypeList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MeetingTypeList = () => {
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+        
+    </section>
+  )
+}
+
+export default MeetingTypeList
